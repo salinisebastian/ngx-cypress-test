8f747680f8729ab94dfadf39ba266ca934a2a52c
salinisebastian <salini.seb@gmail.com>
2021-02-27 22:24:39 +0100

    Different ways to find webelements

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,32 @@
+///<reference types="cypress"/>
+describe("Our first suite", () => {
+  it("first test", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+    //by tag name
+    cy.get("input");
+
+    //by ID
+    cy.get("#inputEmail1");
+
+    //by classname
+    cy.get(".input-full-width");
+
+    //by attribute name
+    cy.get("[placeholder]");
+
+    //by attribute name and value
+    cy.get('[placeholder="Email"]');
+
+    //by class value
+    cy.get('[class="input-full-width size-medium shape-rectangle"]');
+
+    //by tagname and attribute with value
+    cy.get('input[placeholder="Email"]');
+
+    //by two different attributes
+    cy.get('[placeholder="Email"][fullwidth]');
+    // or cy.get('[placeholder="Email"][type="email"]')
+  });
+});
